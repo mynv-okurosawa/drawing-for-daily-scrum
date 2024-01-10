99392e01a2793da6512a892d1417dc58a142ec73
YutoKuboKubo <kubo.yuto.ik@mynavi.jp>
2024-01-10 17:17:11 +0900

    人を変更

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 // くじ引き結果
 const results = [
-    "鈴木 拓暁",
-    "多田 康造",
-    "黒澤 治",
-    "佐々木 知里",
-    "久保 優斗",
+  "鈴木 晶帆",
+  "多田 康造",
+  "黒澤 治",
+  "佐々木 知里",
+  "久保 優斗",
 ];
 
 function getResult() {
-    var random = Math.floor( Math.random() * results.length );
-    var result =  results[random];
-    $('#result').html(result);
+  var random = Math.floor(Math.random() * results.length);
+  var result = results[random];
+  $("#result").html(result);
 }
